33255f7c4a6f3c5d1a0c1ae3de575e1e6921e346
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-11 17:03:51 +0100

    add nav

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,14 +1,18 @@
 <header>
-    <nav>
-        <img src="{{asset('img/dc-logo.png')}}" alt="">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </nav>
+    <div class="container-h">
+        <nav>
+            <ul>
+                <li><a href=""><img src="{{asset('img/dc-logo.png')}}" alt=""></a></li>
+                <li><a href="">Characters</a></li>
+                <li><a href="">Comics</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">Tv</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Collectibles</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Shop</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
