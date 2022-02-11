95ca3370dc7634006e6cd591970e61774ccf05d4
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-11 16:47:50 +0100

    add logo

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    <div>ciao</div>
+    <nav>
+        <img src="{{asset('img/dc-logo.png')}}" alt="">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
 </header>
