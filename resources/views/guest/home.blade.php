083a7fd2ee1eb8fdc0d0f6bc64c60a2cd77f75a4
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-14 11:50:36 +0100

    add navmain

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -15,6 +15,41 @@
             </div>
         @endforeach
     </div>
+    <div class="jumbonav">
+        <ul>
+            <a href="">
+                <li>
+                     <img src="../../../img/buy-comics-digital-comics.png" alt="">
+                     <span>DIGITAL COMICS</span>
+                </li>
+            </a>
+            <a href="">
+                <li>
+                     <img src="../../../img/buy-comics-merchandise.png" alt="">
+                     <span>DC MERCHANDISE</span>
+                </li>
+             </a>
+            <a href="">
+                <li>
+                     <img src="../../../img/buy-comics-subscriptions.png" alt="">
+                     <span>SUBSCRIPTION</span>
+                </li>
+            </a>
+            <a href="">
+                <li>
+                     <img src="../../../img/buy-comics-shop-locator.png" alt="" class="shop">
+                     <span>COMIC SHOP LOCATOR</span>
+                </li>
+            </a>
+            <a href="">
+                <li>
+                     <img src="../../../img/buy-dc-power-visa.svg" alt="">
+                     <span>DC POWER VISA</span>
+                </li>
+            </a>
+        </ul>
+
+    </div>
 </main>
 
 @endsection
